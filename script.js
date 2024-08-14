b8a88560ee4c4506c260128a0535e761a01b3480
Goose <176062081+wildrobotgoose@users.noreply.github.com>
2024-08-14 10:19:11 -0400

    Show form to add new book when button is clicked

--- script.js
+++ script.js
@@ -35,3 +35,61 @@ function displayBooks() {
 }
 
 displayBooks();
+
+const newBookBtn = document.querySelector(".new-book");
+newBookBtn.addEventListener("click", () => {
+    const newBookForm = document.createElement("div");
+
+    const titleField = document.createElement("input");
+    titleField.setAttribute("type", "text");
+    titleField.setAttribute("id", "new-book-title");
+    titleField.setAttribute("name", "new-book-title");
+    const titleLabel = document.createElement("label");
+    titleLabel.setAttribute("for", "new-book-title");
+    titleLabel.textContent = "Title: ";
+
+    const authorField = document.createElement("input");
+    authorField.setAttribute("type", "text");
+    authorField.setAttribute("id", "new-book-author");
+    authorField.setAttribute("name", "new-book-author");
+    const authorLabel = document.createElement("label");
+    authorLabel.setAttribute("for", "new-book-author");
+    authorLabel.textContent = "Author: ";
+
+    const pagesField = document.createElement("input");
+    pagesField.setAttribute("type", "number");
+    pagesField.setAttribute("id", "new-book-pages");
+    pagesField.setAttribute("name", "new-book-pages");
+    const pagesLabel = document.createElement("label");
+    pagesLabel.setAttribute("for", "new-book-pages");
+    pagesLabel.textContent = "Number of pages: ";
+    newBookForm.append(titleLabel, titleField, authorLabel, authorField,
+                       pagesLabel, pagesField);
+
+    const readRadioBtn = document.createElement("input");
+    readRadioBtn.setAttribute("type", "radio");
+    readRadioBtn.setAttribute("id", "new-book-read");
+    readRadioBtn.setAttribute("name", "new-book-read-status");
+    readRadioBtn.setAttribute("checked", "");
+    const readLabel = document.createElement("label");
+    readLabel.setAttribute("for", "new-book-read");
+    readLabel.textContent = "Read";
+
+    const notReadRadioBtn = document.createElement("input");
+    notReadRadioBtn.setAttribute("type", "radio");
+    notReadRadioBtn.setAttribute("id", "new-book-not-read");
+    notReadRadioBtn.setAttribute("name", "new-book-read-status");
+    const notReadLabel = document.createElement("label");
+    notReadLabel.setAttribute("for", "new-book-not-read");
+    notReadLabel.textContent = "Not read";
+
+    const addBookBtn = document.createElement("button");
+    addBookBtn.setAttribute("class", "add-book");
+    addBookBtn.textContent = "Add";
+
+    newBookForm.append(titleLabel, titleField, authorLabel, authorField,
+        pagesLabel, pagesField, readRadioBtn, readLabel, notReadRadioBtn,
+        notReadLabel, addBookBtn);
+
+    newBookBtn.insertAdjacentElement("afterend", newBookForm);
+});
